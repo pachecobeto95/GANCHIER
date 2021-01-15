9aeb01939b686b911c61a3804f0d6fd568187db7
Roberto Pacheco <robertopvc@gmail.com>
2021-01-15 18:48:42 -0300

    adding zipf distribution to the end node

--- endNode.py
+++ endNode.py
@@ -7,12 +7,17 @@ from routines.sendData import sendImg
 End Device Routine to test Edge device and cloud server
 this file sends an image
 """
+
+
 try:
 	filePath = os.path.join("landmarks", "landmarks", "Query")
 	file_list = os.listdir(filePath) 
+	zipf_idx = np.random.zipf(config.ZIPF_HIGH_PARAMETER, size=1)
 
 	# choice an random file to send to the edge server
-	fileName = file_list[np.random.choice(range(len(file_list)))]
+	# Choose 
+	#fileName = file_list[np.random.choice(range(len(file_list)))]
+	fileName = file_list[zipf_idx]
 	filePath = os.path.join(filePath, fileName)
 	
 	url = config.URL_EDGE + "/api/edge/recognition_cache"
